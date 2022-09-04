5a8610416fb9ece1c57e4942248ccee91095bcfc
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-09-04 19:26:35 -0300

    Added SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
-    <>
-      <h1>My Movies and series</h1>
-    </>
+  <h1 className={styles.title}>
+    My Movies and series
+    </h1>
   )
 }
