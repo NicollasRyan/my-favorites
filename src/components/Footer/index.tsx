06472e3703cc37d57afe8e3168014209371df6c1
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-09-05 19:37:07 -0300

    component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export function Footer() {
+    return (
+        <footer className={styles.footerContainer}>
+            Lista de <span>Nicollas Ryan</span>
+        </footer>
+    )
+}
