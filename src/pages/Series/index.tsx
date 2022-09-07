6c471aec4735e32a3f8382f912ccc797907773d6
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-09-07 09:56:58 -0300

    added series comments and the note

--- src/pages/Series/index.tsx
+++ src/pages/Series/index.tsx
@@ -16,67 +16,60 @@ export default function Series() {
                         <img src="/images/peaky.jpg" alt="GodFather" />
                         <p>
                             Este a Serie foi o mais foda que eu vi. <br />
-                            eu não sabia de nada sobre este filme, nem conhecia o al pacino, então este filme me suprendeu muito <br />
+                            Eu lembro que no final de 2020 estava falndo desta serie, aí eu resovir assisti e foi a melhor desição da minha vida. <br />
+                            Mas a serie conta a historia de Thomas Shelby um cara muito ambisioso que vai fazer de tudo para chegar ao poder <br />
                             <br />
-                            <span>Minha nota 13/10</span>
+                            <span>Minha nota 11/10</span>
                         </p>
                     </div> 
 
                     <div>
                         <img src="/images/bad.jpg" alt="GodFatherII" />
                         <p>
-                            Este filme é sequencia do godfather. <br />
-                            esse filme mostra o Michael corleone tomando os negocíos que seu pai deixou e ele resolve vavios prolhemas da sua familia<br />
+                            Esta serie foi a melhor que eu assisti, mas eu prefiro a de cima. <br />
+                            Sempre fala muito bem desta serie, mas eu evitava de assisti, mas em 2022 eu assisti pra ver se era isso tudo mesmo. <br />
+                            a Historia de um profesor de quimica que pegar câncer e vai produzir metafetamina com seu ex aulo viciado em meta, para deixar um dinhero para sua família<br />
                             <br />
-                            <span>Nota 10/10</span>
+                            <span>Nota 12/10</span>
                         </p>
                     </div>
 
                     <div>
                         <img src="/images/theboys.jpg" alt="Avengers endgame" />
                         <p>
-                            Este filme foi muito emocionate. <br />
-                            Porque é o fim de uma jornada de alguns herois do universo da marvel que eu sempre acompanhei deste crinça. <br />
+                            Essa serie é muito foda. <br />
+                            Essa serie conta como seria os herois na vida real<br />
                             <br />
-                            <span>Nota 10/10</span> 
+                            <span>Nota 9.7/10</span> 
                         </p>
                     </div>
 
                     <div>
                         <img src="/images/saulgoodman.jpg" alt="Avengers inifinity war" />
                         <p>
-                            Já este filme é o mais foda da marvel. <br />
-                            esse filme não é dos herios e sim do thanos e ele tomou conta deste filme <br />
+                            É um derivado de breaking bad. <br />
+                            essa serie conta a historia do Jimmy Mcgill ou Saul good man e tambem mostra o que aconteceu com ele depois de breaking bad <br />
                             <br />
-                            <span>Nota: 10/10</span>
+                            <span>Nota: 9.4/10</span>
                         </p>
                     </div>
 
-                    <div>
-                        <img src="/images/narcos.jpg" alt="Scarface" />
-                        <p>
-                            Scarface é um dos filmes mais fodas que tem. <br />
-                            por conta da historia do Tony Montana um dos caras que foi muito longe com seus negocíos. <br />
-                            <br />
-                            <span>Nota: 9.7/10</span>
-                        </p>
-                    </div>
                     
                     <div>
                         <img src="/images/loki.jpg" alt="Batman the dark knight" />
                         <p>
-                            Este filme do Batmam é um dos melhores. <br />
-                            O coringa tambem toma conta do filme uma das melhores atuação do cinema. <br />
+                            O que aconteceu com o loki depois de vingadores utimato ?. <br />
+                            Loki, Deus da Trapaça, sai da sombra de seu irmão para embarcar em uma aventura que ocorre após os eventos de "Vingadores: Ultimato"<br />
                             <br />
-                            <span>Nota: 9.7/10</span>
+                            <span>Nota: 8.5/10</span>
                         </p>
                     </div>
 
                     <div>
                         <img src="/images/mocn.jpg" alt="The Batman" />
                         <p>
-                            Aqui este filme é um Bruce wayne com dois ano de batmam. <br />
-                            mas este filme só perde pro cavaleiro da trevas porque o coringa foi muito foda. <br />
+                            O Batman da marvel.<br />
+                            Steven Grant e Marc Spector investigam mistérios dos deuses do Egito de dentro do mesmo corpo. <br />
                             <br />
                             <span>9.6/10</span> 
                         </p>
@@ -85,13 +78,22 @@ export default function Series() {
                     <div>
                         <img src="/images/lupin.jpg" alt="Blade runner" />
                         <p>
-                            Este é o melhor filme do Ryan Gosling. <br />
-                            esse filme como ja diz o titulo se passar em 2049 e após descobrir um segredo que ameaça o que resta da sociedade, um novo policial parte em busca de Rick Deckard, que está desaparecido há 30 anos. <br />
+                            O melhor ladrão. <br />
+                            Inspirado pelas aventuras de Arsène Lupin, o ladrão gentil Assane Diop quer se vingar de uma família rica por uma injustiça cometida contra o pai dele. <br />
                             <br />
                             <span>Nota: 9.4/10</span>
                         </p>
                     </div>
 
+                    <div>
+                        <img src="/images/narcos.jpg" alt="Scarface" />
+                        <p>
+                            ANIDA NÂO TERMINEI <br />
+                            mas deve ser muito foda. <br />
+                            <br />
+                            <span>Nota: 9.7/10</span>
+                        </p>
+                    </div>
                 </section>
             </main>
 
